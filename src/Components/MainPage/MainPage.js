88acef21ac29ecc233a498ec32d858536d068be1
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-10-26 20:46:45 -0300

    primeiro react kkkk

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,43 @@
 import React from 'react'
-import {MainContainer, Form, Input} from './styles'
+import { MainContainer, Form, Input } from './styles'
+import { useState } from 'react'
 
 function MainPage() {
+
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState(0)
+  const [email, setEmail] = useState("")
+
+  const letInputName = (event) => {
+    setNome(event.target.value)
+    console.log(nome)
+  }
+  const letInputIdade = (event) => {
+    setIdade(event.target.value)
+    console.log(idade)
+  }
+  const letInputEmail = (event) => {
+    setEmail(event.target.value)
+    console.log(email)
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input onChange={letInputName} value={nome}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input onChange={letInputIdade} value={idade}/>
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input onChange={letInputEmail} value={email}/>
         </label>
-      <button>Enviar dados</button>
+        <button>Enviar dados</button>
       </Form>
     </MainContainer>
   )
